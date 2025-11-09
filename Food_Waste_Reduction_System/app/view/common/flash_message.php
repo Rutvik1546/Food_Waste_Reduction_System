7ef9612decb2bf279be49721397af8dcdb7ef38f
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2025-11-09 11:41:19 +0530

    Delete Food_Waste_Reduction_System directory

--- Food_Waste_Reduction_System/app/view/common/flash_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-// include_once 'core/Config.php';
-
-// if(isset($_SESSION['flash'])) {
-
-    // echo "<script> alert('{$_SESSION['flash']['message']}');</script>";
-    // unset($_SESSION['flash']);
-// } 
-?>
