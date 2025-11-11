db97128851ebb1258879553bd5f5df739e5421c1
Rutvik Mistry <135792228+Rutvik1546@users.noreply.github.com>
2025-11-11 22:15:27 +0530

    Update configuration settings in config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 return [
     'APP_NAME' => 'FOOD-WASTE-REDUCTION-SYSTEM',
-    'BASE_URL' => 'http://localhost/FOOD_WASTE_REDUCTION_SYSTEM',
-    'BASE_PATH' => 'D:/XAMPP/htdocs/FOOD_WASTE_REDUCTION_SYSTEM',
+    'BASE_URL' => 'https://food-waste-reduction-system.great-site.net/',
+    'BASE_PATH' => __DIR__,
 
-    'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_PASSWORD' => '',
-    'DB_NAME' => 'food_waste',
+    'DB_HOST' => 'sql201.infinityfree.com',
+    'DB_USER' => 'if0_40371068',
+    'DB_PASSWORD' => 'nqcBknCyVuZY42S',
+    'DB_NAME' => 'if0_40371068_food_waste',
 
     'SENDER_EMAIL' => 'rutvikmistry8642@gmail.com'
 ];
